cd2c5c43e357db86170160296c4ebfffbed9cf8d
danielcasto <32224738+danielcasto@users.noreply.github.com>
2021-06-28 22:39:09 -0400

    Fixed script value in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = 'AIRPrediction',
     version = '0.0.1',
-    scripts = ['air_forecasting_service1'],
+    scripts = ['air_forecasting_service1.py'],
     url = 'https://github.com/Data-for-Good-by-UF/AIRPrediction',
     license = 'MIT'
 )
