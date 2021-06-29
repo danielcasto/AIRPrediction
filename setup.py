ab7052212e7295773dcdbf8d5f8ed92f81ff4e3c
danielcasto <32224738+danielcasto@users.noreply.github.com>
2021-06-28 22:16:00 -0400

    added url to repo

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     name = 'AIRPrediction',
     version = '0.0.1',
     scripts = ['air_forecasting_service1'],
+    url = 'https://github.com/Data-for-Good-by-UF/AIRPrediction',
     license = 'MIT'
 )
