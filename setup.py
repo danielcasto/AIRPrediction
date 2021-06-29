979a34b1dc19a2b400d57b3ca260b3c5397bec39
danielcasto <32224738+danielcasto@users.noreply.github.com>
2021-06-28 22:11:47 -0400

    added license and changed script in setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(
     name = 'AIRPrediction',
     version = '0.0.1',
-    scripts = ['prediction_models']
+    scripts = ['air_forecasting_service1'],
+    license = 'MIT'
 )
