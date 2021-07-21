edddf655f7ad1d991ed7c0213789ddd9d4b56160
Haotian Wang <haotian_wang@hotmail.com>
2021-07-21 15:43:46 -0400

    Update prophet_model.py

--- Time_Series_Models/prophet_model.py
+++ Time_Series_Models/prophet_model.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from prophet import Prophet
+from datetime import datetime
 
 
 def prophet_prediction(pollutant, city, date):
@@ -27,6 +28,12 @@ def prophet_prediction(pollutant, city, date):
     # create subset of dataset to include only city and column selected for analysis
     new_df = mean_aqi.loc[mean_aqi['City'] == city, ['date', pollutant_choice]]
 
+    date_format = "%Y-%m-%d"
+
+    start_date = datetime.strptime('2016-04-30', date_format)
+    target_date = datetime.strptime(date, date_format)
+    date_difference = target_date - start_date
+
     new_df = new_df.rename(columns={"date": "ds",
                                     pollutant_choice: "y"})
 
@@ -37,7 +44,7 @@ def prophet_prediction(pollutant, city, date):
     prophet_model.fit(new_df)
 
     # the parameter 'periods' represents the number of days you want to predict after 2016-04-30
-    future = prophet_model.make_future_dataframe(periods=2500)
+    future = prophet_model.make_future_dataframe(periods = date_difference.days)
 
     forecast = prophet_model.predict(future)
 
